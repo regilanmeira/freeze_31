6b79175cd9d9abeae5a7c8f3011defbf132a7a4b
regilanmeira <regilan@hotmail.com>
2024-10-16 08:47:25 -0300

    Criação do arquivo usuario Salvar

---
+++ usuario_cadastro_salvar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php include "referencias.php" ?>
+</head>
+<body>
+
+<?php include "header_admin.php"; ?>
+
+<?php include "footer_admin.php"; ?>
+    
+</body>
+</html>
